ec8a737048a90f7fcc1bc9b0a3f8e220f7383cef
Cleydson Júnior <cleydsonjr@gmail.com>
2016-09-14 20:37:56 -0300

    Documentação dos dois primeiros algoritmos

--- src/main/java/br/ufg/cs/cleydsonjr/ProdutoInteirosPositivos.java
+++ src/main/java/br/ufg/cs/cleydsonjr/ProdutoInteirosPositivos.java
@@ -2,6 +2,13 @@ package br.ufg.cs.cleydsonjr;
 
 public class ProdutoInteirosPositivos {
 
+    /**
+     * Método auxiliar para obtenção do produto entre dois inteiro positivos usando apenas somas
+     *
+     * @param a Primeiro fator para o produto. Obrigatoriamente maior que 0.
+     * @param b Segundo fator para o produto. Obrigatoriamente maior que 0.
+     * @return O produto dos dois números fornecidos
+     */
     public int produtoInteirosPositivos(int a, int b) {
         if (a < 0 || b < 0) {
             throw new IllegalArgumentException("Números informados inválidos: " + a + ", " + b);
--- src/main/java/br/ufg/cs/cleydsonjr/SomaNaturais.java
+++ src/main/java/br/ufg/cs/cleydsonjr/SomaNaturais.java
@@ -2,6 +2,12 @@ package br.ufg.cs.cleydsonjr;
 
 public class SomaNaturais {
 
+    /**
+     * Método auxiliar para cálculo dos primeiros números naturais sequenciais até um número inteiro positivo fornecido
+     *
+     * @param n Número limite para o qual se quer obter a soma. Obrigatoriamente maior que 1
+     * @return A soma dos primeiros naturais.
+     */
     public int somaNaturais(int n) {
         if (n < 1) {
             throw new IllegalArgumentException("Número inválido fornecido: " + n);
